52a7f55d45f62e6156628a648815ce326834c48e
Robert Alexander <robalexdr@gmail.com>
2026-07-18 23:34:19 -0700

    created minimal plain html app with link to built resume pdf

---
+++ README.md
@@ -0,0 +1,16 @@
+# Personal site
+
+Personal website for Robert Alexander.
+
+## Development
+
+```sh
+npm install
+npm run dev
+```
+
+Build the site with:
+
+```sh
+npm run build
+```
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="site">
+      <h1>Robert Alexander</h1>
+      <a href="/resume.pdf">Resume</a>
+    </main>
+  )
+}
+
+export default App
